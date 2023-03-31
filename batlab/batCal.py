61cfb852c9a1045b948af92a1f5e193d6946bb64
ConorGover <98365021+ConorGover@users.noreply.github.com>
2023-03-31 18:03:00 -0400

    add test-only option to batCal

--- batlab/batCal.py
+++ batlab/batCal.py
@@ -7,6 +7,7 @@
 #   IMPORTANT: copy of Batlab library obtained from https://github.com/ConorGover/batlab-software-python
 #       The original Lexcelon Batlab library has a bug which will corrupt the calibration values.
 
+import sys
 from time import sleep
 import numpy as np
 from scipy.optimize import minimize
@@ -131,103 +132,105 @@ while v < 3:
         raise Fault('Ensure that Keithley power supply is connected to Batlab.')
 cell = i - 1
 
-sca_old = bl.read(cell,CURRENT_CALIB_SCA).data
-off_old = bl.read(cell,CURRENT_CALIB_OFF).data
-print('old calibration: SCA = ' + str(sca_old) + ', OFF = ' + str(off_old))
-bl.write(cell,MODE,MODE_DISCHARGE)
-test_calibration()
-
-print('setting scale factor to 1 and offset to 0')
-bl.write(cell,CURRENT_CALIB_SCA,0x4000)
-bl.write(cell,CURRENT_CALIB_OFF,0)
-
-bl.write(cell,CURRENT_SETPOINT,batlab.encoder.Encoder(0).assetpoint())
-bl.write(cell,ERROR,0)
-bl.write(cell,MODE,MODE_DISCHARGE)
-
 try:
-    current = min_current
-    while current <= max_current:
-        bl.write(cell,CURRENT_SETPOINT,batlab.encoder.Encoder(current).assetpoint())
-
-        # wait for Batlab's current feedback loop to stabilize
-        prev_duty = 0
-        duty = 1
-        while duty != prev_duty:
-            prev_duty = duty
-            duty = bl.read(cell,DUTY).data
-            sleep(0.2)
-
-        # make sure Batlab is in discharge mode
-        if bl.read(cell,MODE).data != MODE_DISCHARGE:
-            print(bl.read(cell,ERROR).aserr())
-            raise Fault('Something went wrong ¯\_(ツ)_/¯')
-        
-        bat_curr_sum = 0
-        ps_curr_sum = 0
-        for i in range(8):
-            bat_curr_sum += bl.read(cell,CURRENT).ascurrent()
-            ps_curr_sum += ps.get_current()
-            sleep(0.05)
-        bat_current = bat_curr_sum / 8
-        ps_current = ps_curr_sum / 8
-        currents[bat_current] = ps_current
-        if OPTIMIZE_FOR == "ABSOLUTE":
-            error = (bat_current - ps_current)
-            print(f"Batlab says: {bat_current:.3f} A and PS says: {ps_current:.3f} A.   Error: {(error*1000):.0f} mA")
-        elif OPTIMIZE_FOR == "RELATIVE":
-            error = (bat_current - ps_current) / ps_current
-            print(f"Batlab says: {bat_current:.3f} A and PS says: {ps_current:.3f} A.   Error: {error:.3%}")
-
-        if current == max_current:
-            break
-        current = min( (current + step), max_current )
-
-    bat_currents = list(currents.keys())
-    ps_currents = list(currents.values())
-
-    x = np.array(bat_currents)
-    y = np.array(ps_currents)
-    # initial guess for the parameters: scale factor = 1, offset = 0
-    params0 = np.array([1, 0])
-
-    if OPTIMIZE_FOR == "ABSOLUTE":
-        def objective(params, x, y):
-            m, b = params
-            return np.sum(np.abs(y - m*x - b))
-        
-        # since most of the charge/discharge happens at the max current, we'll constrain the line of best fit to go through that point
-        # the last current value was the maximum
-        x0 = bat_current
-        y0 = ps_current
-        def constraint(params, x0, y0):
-            m, b = params
-            return y0 - m * x0 - b
-        constraint_dict = {'type': 'eq', 'fun': constraint, 'args': (x0, y0)}
-        result = minimize(objective, params0, args=(x, y), constraints=constraint_dict)
-
-    elif OPTIMIZE_FOR == "RELATIVE":
-        def objective(params, x, y):
-            m, b = params
-            return np.sum((y - (m * x + b))**2)
-        result = minimize(objective, params0, args=(x, y))
-
-    m, b = result.x
-
-    print('new calibration: scale factor = ' + str(m) + ', offset = ' + str(b))
-    # in the firmware, the scale factor is a divisor, so we need to invert it
-    sca_new = int(0x4000 / m)
-    # the offset is subtracted rather than added, hence the negative sign
-    # note: in the Batlab Encoder library there is an asioff() method, but it is wrong.
-    #   ascurrent() gives the correct value.
-    off_new = batlab.encoder.Encoder(-b).ascurrent()
-    print('new calibration: SCA = ' + str(sca_new) + ', OFF = ' + str(off_new))
-    bl.write(cell,CURRENT_CALIB_SCA,sca_new)
-    bl.write(cell,CURRENT_CALIB_OFF,off_new)
-    sleep(0.1)
-
-    print("Now let's try that again.")
+    sca_old = bl.read(cell,CURRENT_CALIB_SCA).data
+    off_old = bl.read(cell,CURRENT_CALIB_OFF).data
+    print('old calibration: SCA = ' + str(sca_old) + ', OFF = ' + str(off_old))
+    bl.write(cell,MODE,MODE_DISCHARGE)
     test_calibration()
+
+    if sys.argv[1] != '-t':
+        print('setting scale factor to 1 and offset to 0')
+        bl.write(cell,CURRENT_CALIB_SCA,0x4000)
+        bl.write(cell,CURRENT_CALIB_OFF,0)
+
+        bl.write(cell,CURRENT_SETPOINT,batlab.encoder.Encoder(0).assetpoint())
+        bl.write(cell,ERROR,0)
+        bl.write(cell,MODE,MODE_DISCHARGE)
+
+
+        current = min_current
+        while current <= max_current:
+            bl.write(cell,CURRENT_SETPOINT,batlab.encoder.Encoder(current).assetpoint())
+
+            # wait for Batlab's current feedback loop to stabilize
+            prev_duty = 0
+            duty = 1
+            while duty != prev_duty:
+                prev_duty = duty
+                duty = bl.read(cell,DUTY).data
+                sleep(0.2)
+
+            # make sure Batlab is in discharge mode
+            if bl.read(cell,MODE).data != MODE_DISCHARGE:
+                print(bl.read(cell,ERROR).aserr())
+                raise Fault('Something went wrong ¯\_(ツ)_/¯')
+            
+            bat_curr_sum = 0
+            ps_curr_sum = 0
+            for i in range(8):
+                bat_curr_sum += bl.read(cell,CURRENT).ascurrent()
+                ps_curr_sum += ps.get_current()
+                sleep(0.05)
+            bat_current = bat_curr_sum / 8
+            ps_current = ps_curr_sum / 8
+            currents[bat_current] = ps_current
+            if OPTIMIZE_FOR == "ABSOLUTE":
+                error = (bat_current - ps_current)
+                print(f"Batlab says: {bat_current:.3f} A and PS says: {ps_current:.3f} A.   Error: {(error*1000):.0f} mA")
+            elif OPTIMIZE_FOR == "RELATIVE":
+                error = (bat_current - ps_current) / ps_current
+                print(f"Batlab says: {bat_current:.3f} A and PS says: {ps_current:.3f} A.   Error: {error:.3%}")
+
+            if current == max_current:
+                break
+            current = min( (current + step), max_current )
+
+        bat_currents = list(currents.keys())
+        ps_currents = list(currents.values())
+
+        x = np.array(bat_currents)
+        y = np.array(ps_currents)
+        # initial guess for the parameters: scale factor = 1, offset = 0
+        params0 = np.array([1, 0])
+
+        if OPTIMIZE_FOR == "ABSOLUTE":
+            def objective(params, x, y):
+                m, b = params
+                return np.sum(np.abs(y - m*x - b))
+            
+            # since most of the charge/discharge happens at the max current, we'll constrain the line of best fit to go through that point
+            # the last current value was the maximum
+            x0 = bat_current
+            y0 = ps_current
+            def constraint(params, x0, y0):
+                m, b = params
+                return y0 - m * x0 - b
+            constraint_dict = {'type': 'eq', 'fun': constraint, 'args': (x0, y0)}
+            result = minimize(objective, params0, args=(x, y), constraints=constraint_dict)
+
+        elif OPTIMIZE_FOR == "RELATIVE":
+            def objective(params, x, y):
+                m, b = params
+                return np.sum((y - (m * x + b))**2)
+            result = minimize(objective, params0, args=(x, y))
+
+        m, b = result.x
+
+        print('new calibration: scale factor = ' + str(m) + ', offset = ' + str(b))
+        # in the firmware, the scale factor is a divisor, so we need to invert it
+        sca_new = int(0x4000 / m)
+        # the offset is subtracted rather than added, hence the negative sign
+        # note: in the Batlab Encoder library there is an asioff() method, but it is wrong.
+        #   ascurrent() gives the correct value.
+        off_new = batlab.encoder.Encoder(-b).ascurrent()
+        print('new calibration: SCA = ' + str(sca_new) + ', OFF = ' + str(off_new))
+        bl.write(cell,CURRENT_CALIB_SCA,sca_new)
+        bl.write(cell,CURRENT_CALIB_OFF,off_new)
+        sleep(0.1)
+
+        print("Now let's try that again.")
+        test_calibration()
 
 finally:
     # stop discharge and turn off power supply output
